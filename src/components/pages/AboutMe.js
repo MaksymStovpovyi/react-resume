8d60467e527cd4291f97416c0ee2b6a214113351
maxSt <stovpovymax@gmail.com>
2023-03-24 00:03:27 -0400

    about me style

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -2,11 +2,38 @@ import React from 'react';
 
 export default function AboutMe() {
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>
-        about
-      </p>
+    <div className='wrapper'>
+      <div className='container-about'>
+
+        <h2 className='title'>About Me</h2>
+
+        <p>
+          Hello! My name is Maksym Stovpovyi, and I am a passionate web developer with a strong focus on JavaScript.
+      
+          My journey as a web developer started 3 years ago when I discovered my love for coding. Since then, I've been constantly improving my skills and learning new technologies. My expertise lies in building complex web applications using modern front-end framework like React.js.
+        </p>
+        
+        <p>
+          I take pride in my ability to write clean, efficient, and maintainable code. I always strive to follow best practices and write code that is easy to read and understand. I believe that good code is not just about solving the problem at hand but also about making it easy to maintain and extend in the future.
+    
+          One of my strengths is my ability to work well in a team. I believe that good communication and collaboration are key to building successful projects. I am always ready to listen to feedback and share my ideas with others.
+        </p>
+
+        <p>
+          Apart from coding, I enjoy staying up-to-date with the latest trends and advancements in the web development industry. I am always looking for new challenges and opportunities to learn and grow.
+      
+          In my free time, I enjoy hiking and exploring the outdoors. I find that spending time in nature helps me to clear my mind and come up with new ideas.
+        </p>
+
+        <p>
+          Thank you for taking the time to learn more about me. I look forward to the opportunity to work with you and contribute to your team as a web developer.
+        </p>
+
+      </div>
+
+      <div className='footer'>
+        
+      </div>
     </div>
   );
 }
